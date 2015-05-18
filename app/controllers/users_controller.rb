9c84adcd5103c35da249b0c3f83c9792c052cb3e
Surya Wono <suryawono@yahoo.co.id>
2015-05-18 17:48:03 +0700

    add grafik harian

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  
   def dashboard
+    @j = ActiveSupport::JSON
     @title="Dashboard"
-    @tujuh_hari=Transaction.all
+    @tujuh_hari_pemasukan=Transaction.joins(:category).where('tanggal >= ?', Time.zone.now - 7.days).where("categories.category_type_id = 10").group(:tanggal).sum(:nominal)
+    @tujuh_hari_pengeluaran=Transaction.joins(:category).where('tanggal >= ?', Time.zone.now - 7.days).where("categories.category_type_id = 11").group(:tanggal).sum(:nominal)
   end
+  
 end
