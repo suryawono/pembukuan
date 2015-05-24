c2c56dde972f2f22aa9c3f38a8c14160cdd64870
Surya Wono <suryawono@yahoo.co.id>
2015-05-24 22:16:46 +0700

    add another filter

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,9 +5,29 @@ class Transaction < ActiveRecord::Base
   
   filterrific :available_filters => %w[
                 with_category_id
+                minimal_nominal
+                maximal_nominal
+                awal_tanggal
+                akhir_tanggal
   ]
   
   scope :with_category_id, lambda { |category_ids|
     where(:category_id => [*category_ids])
+  }
+  
+  scope :minimal_nominal, lambda{ |minimal_nonimal|
+    where("nominal >= ?",minimal_nonimal)
+  }
+  
+  scope :maximal_nominal, lambda{ |maximal_nominal|
+    where("nominal <= ?",maximal_nominal)
+  }
+  
+   scope :awal_tanggal, lambda{ |awal_tanggal|
+    where("tanggal >= ?",awal_tanggal)
+  }
+  
+  scope :akhir_tanggal, lambda{ |akhir_tanggal|
+    where("tanggal <= ?",akhir_tanggal)
   }
 end
